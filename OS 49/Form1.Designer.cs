e4f2d31dd87296a564733887e59e7c56e56b4ecd
mohan <mohan@MOHANNAD>
2025-12-23 12:36:37 -0500

    Add project files.

---
+++ OS 49/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace OS_49
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            file_man = new Button();
+            sys_info = new Button();
+            SuspendLayout();
+            // 
+            // file_man
+            // 
+            file_man.Location = new Point(298, 170);
+            file_man.Name = "file_man";
+            file_man.Size = new Size(179, 49);
+            file_man.TabIndex = 0;
+            file_man.Text = "File Manager";
+            file_man.UseVisualStyleBackColor = true;
+            // 
+            // sys_info
+            // 
+            sys_info.Location = new Point(298, 281);
+            sys_info.Name = "sys_info";
+            sys_info.Size = new Size(179, 50);
+            sys_info.TabIndex = 1;
+            sys_info.Text = "System Info";
+            sys_info.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(824, 543);
+            Controls.Add(sys_info);
+            Controls.Add(file_man);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button file_man;
+        private Button sys_info;
+    }
+}
---
+++ OS 49/Form2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OS_49
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
